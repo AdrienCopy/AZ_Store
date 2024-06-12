1662aca0395f4d390766e4a7c52dea4cfc42d417
Dj3y <dkyamilova@gmail.com>
2024-06-12 10:10:47 +0200

    ajout structure index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    <header>
+        <p>AZ&#91;store&#93;</p>
+        <nav>
+            <a href="index.php">Home</a>
+            <a href="">About</a>
+            <a href="">Products</a>
+            <a href="">Contact</a>
+        </nav>
+        <div>
+            <img src="assets/picture/shopping-cart.svg" alt="">
+            <p>Login</p>
+        </div>
+    </header>
+    <main></main>
+    <footer></footer>
+</body>
+</html>
