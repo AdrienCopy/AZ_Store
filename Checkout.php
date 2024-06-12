38744358bcd8576da01e3c0deb1b5c38a11ea8e6
Adrien Boels <adrien.boels@gmail.com>
2024-06-12 10:26:33 +0200

    Checkout.php

---
+++ Checkout.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AZ Store</title>
+</head>
+<body>
+    <form action="">
+        <label for="first_name">First name</label>
+        <input type="text">
+        <label for="last_name">Last name</label>
+        <input type="text">
+        <label for="email">Email</label>
+        <input type="text">
+        <label for="adress">Address</label>
+        <input type="text">
+        <label for="city">City</label>
+        <input type="text">
+        <label for="">Zip code</label>
+        <input type="text">
+        <label for="">Country</label>
+        <input type="text">
+    </form>
+</body>
+</html>
